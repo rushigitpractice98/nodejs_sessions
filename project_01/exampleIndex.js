f50d28096e7e2c79020806150611b7e8b17a53f4
rushigithub98 <rushirkamble.in@gmail.com>
2024-02-25 22:49:17 +0530

    postgres setup, CRUD operations

---
+++ project_01/exampleIndex.js
@@ -0,0 +1,108 @@
+import express from "express";
+import users from "./MOCK_DATA_USERS.json" assert { type: "json" };
+import fs from "fs";
+
+const app = express();
+const PORT = 8000;
+
+// Middleware-plugin
+app.use(express.urlencoded({ extended: false }));
+
+//custom middleware
+app.use((req, res, next) => {
+  console.log("M=logged middleware1");
+  fs.appendFile(
+    "log.txt",
+    `\n ${Date.now()}: ${req.method}: ${req.path}`,
+    (err, data) => {
+      next();
+    }
+  );
+});
+
+//this route will return HTML
+app.get("/users", (req, res) => {
+  const html = `
+       <ul>
+         ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+       </ul>
+    `;
+  return res.send(html);
+});
+
+//REST API ENDPONTS
+
+//get all users
+app.get("/api/users", (req, res) => {
+  console.log("reqheader==>>", req.headers);
+  res.setHeader("X-myName", "RushiK"); //custom header starts  with X-...
+  return res.json(users);
+});
+
+
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+
+  if (!user) return res.status(400).json({ message: "User does not exist" });
+
+  return res.json(user);
+});
+
+
+app.post("/api/users", (req, res) => {
+  console.log("requestbody===>>", req.body);
+  if (
+    !req.body.first_name ||
+    !req.body.last_name ||
+    !req.body.email ||
+    !req.body.gender ||
+    !req.body.job_title
+  ) {
+    return res.status(400).json({ message: "All fields are required!!" });
+  }
+  const body = { ...req.body, id: users.length + 1 };
+
+  users.push({ ...body });
+
+  fs.writeFile("./MOCK_DATA_USERS.json", JSON.stringify(users), (err, data) => {
+    return res.status(201).json({ status: "Success", id: users.length });
+  });
+});
+
+app.patch("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const body = { ...req.body };
+
+  const updateduser = users.map((user) => {
+    if (user.id === id) {
+      user = { ...user, ...body };
+    }
+    return user;
+  });
+
+  fs.writeFile(
+    "./MOCK_DATA_USERS.json",
+    JSON.stringify(updateduser),
+    (err, data) => {
+      return res.json({ status: "Success", id });
+    }
+  );
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  console.log("DELETE", req.params);
+  const id = Number(req.params.id);
+  const updatedUsers = users.filter((user) => user.id !== id);
+
+  fs.writeFile(
+    "./MOCK_DATA_USERS.json",
+    JSON.stringify(updatedUsers),
+    (err, data) => {
+      console.log("Error===>>>", err);
+      return res.json({ status: `Deleted user:${id}` });
+    }
+  );
+});
+
+app.listen(PORT, () => console.log("Server started!!!!"));
--- project_01/index.js
+++ project_01/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import users from "./MOCK_DATA_USERS.json" assert { type: "json" };
 import fs from "fs";
+import { db } from "./db.js";
 
 const app = express();
 const PORT = 8000;
@@ -8,91 +9,153 @@ const PORT = 8000;
 // Middleware-plugin
 app.use(express.urlencoded({ extended: false }));
 
-//custom middleware
-app.use((req, res, next) => {
-  console.log("M=logged middleware1");
-  fs.appendFile(
-    "log.txt",
-    `\n ${Date.now()}: ${req.method}: ${req.path}`,
-    (err, data) => {
-      next();
-    }
-  );
-});
-
-//this route will return HTML
-app.get("/users", (req, res) => {
-  const html = `
-       <ul>
-         ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
-       </ul>
-    `;
-  return res.send(html);
-});
-
 //REST API ENDPONTS
-app.get("/api/users", (req, res) => {
-  console.log("reqheader==>>", req.headers);
-  res.setHeader("X-myName", "RushiK"); //custom header starts  with X-...
-  return res.json(users);
-}); //get all users
 
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id);
-
-  if(!user) return res.status(400).json({ message: "User does not exist" })
-
-  return res.json(user);
-});
-
-app.post("/api/users", (req, res) => {
-  console.log("requestbody===>>", req.body);
-  if(!req.body.first_name ||  !req.body.last_name || !req.body.email || !req.body.gender || !req.body.job_title){
-    return res.status(400).json({ message: "All fields are required!!" })
+//get all users
+app.get("/api/users", async (req, res) => {
+  try {
+    const client = await db.connect();
+    try {
+      const result = await client.query("SELECT * FROM users");
+      const users = result.rows;
+      res.json(users);
+    } finally {
+      client.release();
+    }
+  } catch (err) {
+    console.error("Error executing query", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-  const body = { ...req.body, id: users.length + 1 };
-
-  users.push({ ...body });
-
-  fs.writeFile("./MOCK_DATA_USERS.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({ status: "Success", id: users.length });
-  });
 });
 
-app.patch("/api/users/:id", (req, res) => {
+//get user by ID
+app.get("/api/users/:id", async (req, res) => {
   const id = Number(req.params.id);
-  const body = { ...req.body };
 
-  const updateduser = users.map((user) => {
-    if (user.id === id) {
-      user = { ...user, ...body };
-    }
-    return user;
-  });
+  try {
+    const client = await db.connect();
+    try {
+      const result = await client.query("SELECT * FROM users WHERE id = $1", [
+        id,
+      ]);
+      const user = result.rows[0];
 
-  fs.writeFile(
-    "./MOCK_DATA_USERS.json",
-    JSON.stringify(updateduser),
-    (err, data) => {
-      return res.json({ status: "Success", id });
+      if (!user)
+        return res.status(400).json({ message: "User does not exist" });
+
+      res.json(user);
+    } finally {
+      client.release();
     }
+  } catch (err) {
+    console.error("Error executing query", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//POST request
+app.post("/api/users", async (req, res) => {
+  try {
+    const { first_name, last_name, email, gender, job_title } = req.body;
+
+    // Check if all required fields are provided
+    if (!first_name || !last_name || !email || !gender || !job_title) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const client = await db.connect();
+
+    try {
+      const result = await client.query(
+        "INSERT INTO users (first_name, last_name, email, gender, job_title) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [first_name, last_name, email, gender, job_title]
+      );
+
+      const newUser = result.rows[0];
+
+      res.status(201).json(newUser);
+    } finally {
+      client.release();
+    }
+  } catch (err) {
+    console.error("Error creating user", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//PATCH request
+app.patch("/api/users/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const { first_name, last_name, email, gender, job_title } = req.body;
+
+  const updates = { first_name, last_name, email, gender, job_title };
+
+  const filteredUpdates = Object.fromEntries(
+    Object.entries(updates).filter(
+      ([key, value]) => value !== undefined && value !== null
+    )
   );
+
+  if (Object.keys(filteredUpdates).length === 0) {
+    return res.status(400).json({ message: "No fields to update" });
+  }
+
+  try {
+    const client = await db.connect();
+    try {
+      const setClause = Object.entries(filteredUpdates)
+        .map(([key, value], index) => `${key} = $${index + 1}`)
+        .join(", ");
+
+      const values = Object.values(filteredUpdates);
+
+      values.push(id);
+
+      const result = await client.query(
+        `UPDATE users SET ${setClause} WHERE id = $${values.length} RETURNING *`,
+        values
+      );
+
+      const updatedUser = result.rows[0];
+
+      if (!updatedUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.json(updatedUser);
+    } finally {
+      client.release();
+    }
+  } catch (err) {
+    console.error("Error updating user", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.delete("/api/users/:id", (req, res) => {
-  console.log("DELETE", req.params);
-  const id = Number(req.params.id);
-  const updatedUsers = users.filter((user) => user.id !== id);
 
-  fs.writeFile(
-    "./MOCK_DATA_USERS.json",
-    JSON.stringify(updatedUsers),
-    (err, data) => {
-      console.log("Error===>>>", err);
+//delete request
+app.delete("/api/users/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const client = await db.connect();
+    try {
+      const result = await client.query("DELETE FROM users WHERE id = $1", [
+        id,
+      ]);
+
+      if (result.rowCount === 0) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
       return res.json({ status: `Deleted user:${id}` });
+    } finally {
+      client.release();
     }
-  );
+  } catch (err) {
+    console.error("Error deleting user", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(PORT, () => console.log("Server started!!!!"));
